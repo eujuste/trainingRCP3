5a534d8c57c4796d685e93da69163d236d381ff3
jplakoo <jplakoo@LFR000548.corp.capgemini.com>
2017-03-08 10:01:05 +0100

    fin windowBulder

--- com.sogeti.rental.ui/src/com/sogeti/rental/ui/views/RentalPropertyView.java
+++ com.sogeti.rental.ui/src/com/sogeti/rental/ui/views/RentalPropertyView.java
@@ -1,9 +1,6 @@
 package com.sogeti.rental.ui.views;
 
 
-import java.security.Principal;
-import java.util.Enumeration;
-
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -12,13 +9,17 @@ import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.part.ViewPart;
 
+import com.opcoach.training.rental.Rental;
+import com.sogeti.rental.core.RentalCoreActivator;
+
 public class RentalPropertyView extends ViewPart {
 	
 	Label rentalLabel;
-	Label rentalObjet1;
-	Label rentalObjet2;
+	
 	Label rentalCustomer1;
-	Label rentalCustomer2;
+	private Group grpDateDeLocation;
+	Label startDate;
+	Label endDate;
 
 	public RentalPropertyView() {
 		// TODO Auto-generated constructor stub
@@ -31,22 +32,34 @@ public class RentalPropertyView extends ViewPart {
 		Group infogroup = new Group(parent,SWT.NONE);
 		infogroup.setText("Informations");
 		infogroup.setLayout(new GridLayout(2,false));
-		rentalLabel = new Label(parent, SWT.BORDER);
+		rentalLabel = new Label(infogroup, SWT.BORDER);
 		GridData gd = new GridData();
 		gd.horizontalAlignment = SWT.FILL;
 		gd.horizontalSpan = 2;
 		rentalLabel.setLayoutData(gd);
 		
-		rentalObjet1 = new Label(infogroup, SWT.BORDER);
-		rentalObjet1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		rentalObjet2 = new Label(infogroup, SWT.BORDER);
-		rentalObjet2.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		 new Label(infogroup, SWT.BORDER).setText("Louer à:");
+		
+	
 		rentalCustomer1 = new Label(infogroup, SWT.BORDER);
 		rentalCustomer1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		rentalCustomer2 = new Label(infogroup, SWT.BORDER);
-		rentalCustomer2.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-
 		
+		grpDateDeLocation = new Group(parent, SWT.NONE);
+		grpDateDeLocation.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		grpDateDeLocation.setText("Date de location");
+		grpDateDeLocation.setLayout(new GridLayout(2, false));
+		
+		Label lblNewLabel_2 = new Label(grpDateDeLocation, SWT.NONE);
+		lblNewLabel_2.setText("Start date:");
+		
+		startDate = new Label(grpDateDeLocation, SWT.NONE);
+		
+		Label lblNewLabel = new Label(grpDateDeLocation, SWT.NONE);
+		lblNewLabel.setText("End date:");
+		
+		endDate = new Label(grpDateDeLocation, SWT.NONE);
+		
+		setRental(RentalCoreActivator.getAgency().getRentals().get(0));
 		
 		
 		
@@ -57,5 +70,13 @@ public class RentalPropertyView extends ViewPart {
 		// TODO Auto-generated method stub
 
 	}
-
+	
+	public void setRental(Rental r)
+	{
+		
+		rentalLabel.setText(r.getRentedObject().getName());
+		rentalCustomer1.setText(r.getCustomer().getDisplayName());
+		startDate.setText(r.getStartDate().toString());
+		endDate.setText(r.getEndDate().toString());
+	}
 }
